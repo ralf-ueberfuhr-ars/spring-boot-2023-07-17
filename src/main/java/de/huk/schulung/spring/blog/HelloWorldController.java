c85bd6bb4a2c16c71b4d263da0c7dd87e6fcd157
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-07-17 15:24:28 +0200

    Add HelloWorld controller

---
+++ src/main/java/de/huk/schulung/spring/blog/HelloWorldController.java
@@ -0,0 +1,28 @@
+package de.huk.schulung.spring.blog;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping (produces = MediaType.TEXT_PLAIN_VALUE)// "hello"
+    public String sayHello(
+            @RequestParam(name="name", defaultValue = "World")
+            String name
+    ) {
+        return "<h1>Hello " + name + "</h1>";
+    }
+
+    @GetMapping("/2") // "hello/2"
+    public String sayHello2() {
+        return "";
+    }
+
+}
