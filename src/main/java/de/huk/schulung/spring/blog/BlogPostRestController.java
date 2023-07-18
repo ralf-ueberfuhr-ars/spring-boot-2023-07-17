a1683b64c001a6fe7fa2e02a7f9688e986f2ffeb
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-07-18 15:57:22 +0200

    Add DELETE endpoint and add location header to POST endpoint's 201 response

--- src/main/java/de/huk/schulung/spring/blog/BlogPostRestController.java
+++ src/main/java/de/huk/schulung/spring/blog/BlogPostRestController.java
@@ -3,12 +3,16 @@ package de.huk.schulung.spring.blog;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Validated // non-REST: for validating request parameters instead of request bodies
 @RestController
@@ -18,8 +22,8 @@ public class BlogPostRestController {
     private final Collection<BlogPost> posts = new HashSet<>();
 
     {
-    posts.add(new BlogPost(1L, "Spring is toll!", "lorem ipsum", LocalDateTime.now()));
-    posts.add(new BlogPost(2L, "Lombok is toll!", "lorem ipsum", LocalDateTime.now()));
+        posts.add(new BlogPost(1L, "Spring is toll!", "lorem ipsum", LocalDateTime.now()));
+        posts.add(new BlogPost(2L, "Lombok is toll!", "lorem ipsum", LocalDateTime.now()));
     }
 
     private static long counter = 3L;
@@ -34,21 +38,27 @@ public class BlogPostRestController {
         return posts.stream()
                 .filter(p -> p.getId().equals(id))
                 .findFirst()
-                .get(); // TODO NoSuchElementException -> 404
+                .get();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public BlogPost createBlogPost(@Valid @RequestBody BlogPost newPost) {
+    public ResponseEntity<BlogPost> createBlogPost(@Valid @RequestBody BlogPost newPost) {
         newPost.setId(counter++);
         newPost.setCreationDate(LocalDateTime.now());
         this.posts.add(newPost);
-        return newPost;
-        // REST: HTTP 201 Created mit Location-Header -> ResponseEntity
+        final var location = linkTo(
+                methodOn(BlogPostRestController.class)
+                        .findBlogPostById(newPost.getId())
+        ).toUri();
+        return ResponseEntity.created(location).body(newPost);
     }
 
-    // DELETE? -> DELETE /api/v1/blogposts/3 -> 204 (leerer Body)
-
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBlogPost(@PathVariable("id") long id) {
+        this.posts.remove(this.findBlogPostById(id));
+    }
 
     // KEIN REST!!!
     @PostMapping(value = "/anlegen", produces = MediaType.TEXT_PLAIN_VALUE)
