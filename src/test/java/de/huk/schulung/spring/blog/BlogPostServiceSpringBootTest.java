e3e66ff845b99219e17a9d55fc2224e8cebfd228
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-07-20 14:50:03 +0200

    Add event handler for blog post created event

--- src/test/java/de/huk/schulung/spring/blog/BlogPostServiceSpringBootTest.java
+++ src/test/java/de/huk/schulung/spring/blog/BlogPostServiceSpringBootTest.java
@@ -4,20 +4,38 @@ import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class BlogPostServiceSpringBootTest {
 
     @Autowired
     BlogPostService service;
+    @MockBean
+    BlogPostCreatedLogger logger;
 
     @Test
     void shouldThrowValidationExceptionOnInvalidBlogPostCreation() {
         BlogPost post = new BlogPost();
         assertThatThrownBy(() ->  service.createPost(post))
                 .isInstanceOf(ConstraintViolationException.class);
+        verify(logger, never()).logBlogPostCreated(any());
     }
+
+    @Test
+    void shouldCreateBlogPostAndNotifyLogger() {
+        BlogPost post = new BlogPost();
+        post.setTitle("Title");
+        post.setContent("content");
+        service.createPost(post);
+        verify(logger).logBlogPostCreated(post);
+    }
+
+    // TODO @RecordApplicationEvents
 
 }
