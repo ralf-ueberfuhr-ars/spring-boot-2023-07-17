134a0bf111bb3ca983d9a691bcc0d172c30a1d70
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-07-21 11:33:07 +0200

    Prepare Domain for Persistence Layer

--- src/main/java/de/huk/schulung/spring/blog/persistence/BlogPostSinkDatabaseImpl.java
+++ src/main/java/de/huk/schulung/spring/blog/persistence/BlogPostSinkDatabaseImpl.java
@@ -2,29 +2,36 @@ package de.huk.schulung.spring.blog.persistence;
 
 import de.huk.schulung.spring.blog.domain.BlogPost;
 import de.huk.schulung.spring.blog.domain.BlogPostSink;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Optional;
 
-// TODO weitermachen mit DB
+@Component
+@RequiredArgsConstructor
 public class BlogPostSinkDatabaseImpl implements BlogPostSink {
+
+    private final BlogPostRepository repo;
+
     @Override
     public Collection<BlogPost> findPosts() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public Optional<BlogPost> findPostById(long id) {
-        return Optional.empty();
+        return repo.findById(id);
     }
 
     @Override
     public void savePost(BlogPost newPost) {
-
+        final var savedPost = repo.save(newPost);
+        newPost.setId(savedPost.getId());
     }
 
     @Override
     public void deletePost(long id) {
-
+        repo.deleteById(id);
     }
 }
--- src/test/java/de/huk/schulung/spring/blog/domain/BlogPostServiceSpringBootTest.java
+++ src/test/java/de/huk/schulung/spring/blog/domain/BlogPostServiceSpringBootTest.java
@@ -3,15 +3,18 @@ package de.huk.schulung.spring.blog.domain;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 public class BlogPostServiceSpringBootTest {
 
     @Autowired
@@ -22,7 +25,7 @@ public class BlogPostServiceSpringBootTest {
     @Test
     void shouldThrowValidationExceptionOnInvalidBlogPostCreation() {
         BlogPost post = new BlogPost();
-        assertThatThrownBy(() ->  service.createPost(post))
+        assertThatThrownBy(() -> service.createPost(post))
                 .isInstanceOf(ConstraintViolationException.class);
         verify(logger, never()).logBlogPostCreated(any());
     }
@@ -33,6 +36,13 @@ public class BlogPostServiceSpringBootTest {
         post.setTitle("Title");
         post.setContent("content");
         service.createPost(post);
+        // read post again
+        assertThat(service.getPosts())
+                .hasSize(1)
+                .first()
+                .extracting(BlogPost::getTitle, BlogPost::getContent)
+                .containsExactly("Title", "content");
+        // verify logging
         verify(logger).logBlogPostCreated(post);
     }
 
